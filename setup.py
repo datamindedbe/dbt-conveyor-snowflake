089485cd05cdf2c1d97ab1568abf11fd10219aed
Stijn De Haes <stijndehaes@gmail.com>
2024-01-10 15:50:37 +0000

    Added publishing to pypi (#1)

--- setup.py
+++ setup.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python
 from setuptools import find_namespace_packages, setup
 
-package_name = "dbt-conveyorsnowflake"
+package_name = "dbt-conveyor-snowflake"
 # make sure this always matches dbt/adapters/{adapter}/__version__.py
-package_version = "1.7.0"
 description = """The ConveyorSnowflake adapter plugin for dbt"""
 
 setup(
     name=package_name,
-    version=package_version,
     description=description,
+    setuptools_git_versioning={
+        "enabled": True,
+    },
     long_description=description,
     author="Stijn De Haes",
     author_email="stijn.dehaes@dataminded.be",
-    url="If you have already made a github repo to tie the project to place it here, otherwise update in setup.py later.",
+    url="https://github.com/datamindedbe/dbt-conveyor-snowflake",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
         "dbt-core~=1.7.0",
         "dbt-snowflake~=1.7.0",
     ],
+    setup_requires=["setuptools-git-versioning<2"],
 )
