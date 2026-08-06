a528af64fe1c1f1c29af4272c5f8421207ed72fe
Stijn De Haes <stijndehaes@gmail.com>
2026-08-06 09:46:15 +0100

    Added snowflake role support (#11)

--- dbt/adapters/conveyorsnowflake/connections.py
+++ dbt/adapters/conveyorsnowflake/connections.py
@@ -9,19 +9,27 @@ class ConveyorSnowflakeAdapterCredentials(SnowflakeCredentials):
     # We need to set the default to empty string, otherwise dbt run will complain user is not set and fail
     user: str = field(default='')
     def __post_init__(self):
+        if self.oauth_client_id is None:
+            raise Exception("OAuth client ID is required")
+        if self.oauth_client_secret is None:
+            raise Exception("OAuth client secret is required")
+        args = [
+            "conveyor", "ide", "snowflake-token",
+            f"--snowflake-client-id={self.oauth_client_id}",
+            f"--snowflake-client-secret={self.oauth_client_secret}",
+            f"--snowflake-account={self.account}",
+            "--quiet",
+        ]
+        if self.role:
+            args.append(f"--snowflake-role={self.role}")
         token_output = subprocess.run(
-            [
-                "conveyor", "ide", "snowflake-token",
-                f"--snowflake-client-id={self.oauth_client_id}",
-                f"--snowflake-client-secret={self.oauth_client_secret}",
-                f"--snowflake-account={self.account}",
-                "--quiet",
-            ],
+            args,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT,
         )
-        self.token = json.loads(token_output.stdout.rstrip().decode())["refresh_token"]
-        self.user = json.loads(token_output.stdout.rstrip().decode())["username"]
+        last_line = token_output.stdout.rstrip().decode().splitlines()[-1]
+        self.token = json.loads(last_line)["refresh_token"]
+        self.user = json.loads(last_line)["username"]
         super().__post_init__()
 
     @property
